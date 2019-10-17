f0f4b2588825f82eaac52a9c91c06cda78957403
Javier V <j.vargas.g.d@gmail.com>
2019-10-17 11:13:52 +0200

    Cleaned code of custom training loop

--- keras_custom_training_loop.py
+++ keras_custom_training_loop.py
@@ -5,12 +5,12 @@ from tqdm import tqdm
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.layers import Input, Dense
-from IPython import embed
 
-# Setting seeds
+# Setting seeds for reproducibility 
 np.random.seed(0)
 tf.set_random_seed(0)
 
+# Dataset: given 2 numbers, predict the sum
 # Sum 2 numbers from 0 to 10 dataset
 samples = np.random.randint(0, 9, size=(100,2))
 targets = np.sum(samples, axis=-1)
@@ -19,30 +19,40 @@ targets = np.sum(samples, axis=-1)
 samples_test = np.random.randint(0, 9, size=(10,2))
 targets_test = np.sum(samples_test, axis=-1)
 
-
-# Features
-features = samples.shape[1]
-
 # Model
-x = Input(shape=[features])
+x = Input(shape=[2])
 y = Dense(units=1)(x)
 model = Model(x, y)
 
 # Loss
 def loss_fn(y_true, y_pred):
+    # You can get all the crazy and twisted you 
+    # want here no Keras restrictions this time :)
     loss_value = K.sum(K.pow((y_true - y_pred), 2))
     return loss_value
 
-# Compiling
+# Optimizer to run the gradients
 optimizer = Adam(lr=1e-4)
 
 # Graph creation
-# Creatin training graphs
+# Creating training flow
+# Ground truth input, samples or X_t
 y_true = Input(shape=[0])
-y_pred = model(x)
-loss = K.sum(K.pow((y_true - y_pred), 2))
-updates_op = optimizer.get_updates(params=model.trainable_weights, loss=loss)
 
+# Prediction
+y_pred = model(x)
+
+# Loss 
+loss = loss_fn(y_true, y_pred)
+
+# Operation fort getting 
+# gradients and updating weights
+updates_op = optimizer.get_updates(
+    params=model.trainable_weights, 
+    loss=loss)
+
+# The graph is created, now we need to call it
+# this would be similar to tf session.run()
 train = K.function(
     inputs=[x, y_true], 
     outputs=[loss], 
@@ -52,15 +62,20 @@ test = K.function(
     inputs=[x, y_true], 
     outputs=[loss])
 
-# embed()
 
 # Training loop
 epochs = 100
 
 for epoch in range(epochs):
     print('Epoch %s:' % epoch)
+
+    # Fancy progress bar
     pbar = tqdm(range(len(samples)))
+
+    # Storing losses for computing mean
     losses_train = []
+
+    # Batch loop: batch size=1
     for idx in pbar:
         sample = samples[idx]
         target = targets[idx]
@@ -69,11 +84,12 @@ for epoch in range(epochs):
         sample = np.expand_dims(sample, axis=0)
         target = np.expand_dims(target, axis=0)
 
-        # To tensors
+        # To tensors, input of 
+        # K.function must be tensors
         sample = K.constant(sample)
         target = K.constant(target)
 
-        # Evaluation of train graph
+        # Running the train graph
         loss_train = train([sample, target])
         
         # Compute loss mean
@@ -81,7 +97,7 @@ for epoch in range(epochs):
         loss_train_mean = np.mean(losses_train)
         
         # Update progress bar
-        pbar.set_description('Train Loss %s' % loss_train_mean)
+        pbar.set_description('Train Loss: %.3f' % loss_train_mean)
 
     # Testing
     losses_test = []
@@ -99,11 +115,9 @@ for epoch in range(epochs):
         
         # Evaluation test graph
         loss_test = test([sample_test, target_test])
-        # print("Test Loss: %s" % loss_test[0])
-        # print("Test: sample: %s Pred: %s" % (K.eval(sample_test), K.eval(target_test)))
         
         # Compute test loss mean
         losses_test.append(loss_test[0])
     
     loss_test_mean = np.mean(losses_test)
-    print('Test Loss: %s' % loss_test_mean)
+    print('Test Loss: %.3f' % loss_test_mean)
